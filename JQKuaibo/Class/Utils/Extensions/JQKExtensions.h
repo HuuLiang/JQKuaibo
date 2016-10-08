975aea920f2a9894b7429e53dc32cf6cbb09f058
ylz <yangliuzhong@ylz-2.local>
2016-10-08 15:37:47 +0800

    update qbpayment

--- JQKuaibo/Class/Utils/Extensions/JQKExtensions.h
+++ JQKuaibo/Class/Utils/Extensions/JQKExtensions.h
@@ -9,17 +9,17 @@
 #ifndef JQKExtensions_h
 #define JQKExtensions_h
 
-#import "NSObject+Properties.h"
-#import "NSString+md5.h"
-#import "UIImage+color.h"
+#import <NSObject+Properties.h>
+#import <NSString+md5.h>
+#import <UIImage+color.h>
 #import "UIScrollView+Refresh.h"
-#import "UIColor+hexColor.h"
-#import "NSMutableDictionary+SafeCoding.h"
+#import <UIColor+hexColor.h>
+#import <NSMutableDictionary+SafeCoding.h>
 //#import "NSDictionary+JQKSign.h"
 #import "UIView+Loading.h"
 #import "UIColor+FeaturedColor.h"
 #import "UITableView+Separator.h"
-#import "NSObject+DictionaryRepresentation.h"
-#import "NSString+crypt.h"
+#import <NSObject+DictionaryRepresentation.h>
+#import <NSString+crypt.h>
 
 #endif /* JQKExtensions_h */
--- JQKuaibo/JQKConfig.h
+++ JQKuaibo/JQKConfig.h
@@ -15,7 +15,7 @@
 #define JQK_REST_APP_ID          @"QUBA_2004"
 #define JQK_REST_PV              @113
 #define JQK_PAYMENT_PV           @111//@"103"
-#define JQK_PACKAGE_CERTIFICATE  @"iPhone Distribution: Neijiang Fenghuang Enterprise (Group) Co., Ltd."
+#define JQK_PACKAGE_CERTIFICATE  @"iPhone Distribution: Beijing Huazhong Hengtai Network Technology Co., Ltd."
 #define JQK_PAYMENT_RESERVE_DATA [NSString stringWithFormat:@"%@$%@", JQK_REST_APP_ID, JQK_CHANNEL_NO]
 
 #define JQK_BASE_URL             @"http://iv.zcqcmj.com"//@"http://120.24.252.114:8093" //
