41528d29e8aec0e8ee5f01ca6701df67930b81c6
Liang <q93867@yeah.net>
2016-12-08 19:40:41 +0800

    增加WJPay ZhangPay 微信默认支付配置为WJPay

--- JQKuaibo/JQKConfig.h
+++ JQKuaibo/JQKConfig.h
@@ -14,7 +14,7 @@
 #define JQK_CHANNEL_NO           [JQKConfiguration sharedConfig].channelNo
 #define JQK_REST_APP_ID          @"QUBA_2004"
 #define JQK_REST_PV              @113
-#define JQK_PAYMENT_PV           @200//@113//@"103"
+#define JQK_PAYMENT_PV           @202//@113//@"103"
 #define JQK_PACKAGE_CERTIFICATE  @"iPhone Distribution: Beijing Huazhong Hengtai Network Technology Co., Ltd."
 #define JQK_PAYMENT_RESERVE_DATA [NSString stringWithFormat:@"%@$%@", JQK_REST_APP_ID, JQK_CHANNEL_NO]
 
